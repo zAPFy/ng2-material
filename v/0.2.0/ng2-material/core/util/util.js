2233acb7ca6eb1906c07d144e3e670c02e8f4fb8
Justin DuJardin <justin@dujardinconsulting.com>
2016-01-31 16:45:24 -0800

    chore: 0.2.0

---
+++ v/0.2.0/ng2-material/core/util/util.js
@@ -0,0 +1,40 @@
+var lang_1 = require("angular2/src/facade/lang");
+var lang_2 = require("angular2/src/facade/lang");
+function debounce(func, wait, scope) {
+    var timer;
+    return function debounced() {
+        var context = scope, args = Array.prototype.slice.call(arguments);
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            timer = undefined;
+            func.apply(context, args);
+        }, wait || 10);
+    };
+}
+exports.debounce = debounce;
+function throttle(func, delay, scope) {
+    var recent;
+    return function throttled() {
+        var context = scope;
+        var args = arguments;
+        var now = new Date().getTime();
+        if (!recent || (now - recent > delay)) {
+            func.apply(context, args);
+            recent = now;
+        }
+    };
+}
+exports.throttle = throttle;
+function rAF(callback) {
+    window.requestAnimationFrame(callback);
+}
+exports.rAF = rAF;
+function parseTabIndexAttribute(attr) {
+    return lang_1.isPresent(attr) ? lang_2.NumberWrapper.parseInt(attr, 10) : 0;
+}
+exports.parseTabIndexAttribute = parseTabIndexAttribute;
+function isNumber(value) {
+    return Object.prototype.toString.call(value) === '[object Number]';
+}
+exports.isNumber = isNumber;
+//# sourceMappingURL=util.js.map
