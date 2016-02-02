afe4737114fd8cfdcdd708264d1cb353191d8e51
Justin DuJardin <justin@dujardinconsulting.com>
2016-02-01 18:01:29 -0800

    chore: 0.2.1

---
+++ v/0.2.1/examples/app.js
@@ -0,0 +1,99 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require("angular2/core");
+var browser_1 = require("angular2/platform/browser");
+var router_1 = require("angular2/router");
+var all_1 = require("../ng2-material/all");
+var all_2 = require("./all");
+var example_1 = require("./example");
+var http_1 = require("angular2/http");
+var index_1 = require("./routes/index");
+var component_1 = require("./routes/component");
+var components_1 = require("./services/components");
+var navigation_1 = require("./services/navigation");
+var version_1 = require("./services/version");
+var view_listener_1 = require("angular2/src/core/linker/view_listener");
+var common_dom_1 = require("angular2/platform/common_dom");
+var sidenav_service_1 = require("ng2-material/components/sidenav/sidenav_service");
+var media_1 = require("ng2-material/core/util/media");
+var DemosApp = (function () {
+    function DemosApp(http, navigation, media, router, appRef, _components, _sidenav) {
+        var _this = this;
+        this.navigation = navigation;
+        this.media = media;
+        this.router = router;
+        this.appRef = appRef;
+        this._components = _components;
+        this._sidenav = _sidenav;
+        this.fullPage = media_1.Media.hasMedia(DemosApp.SIDE_MENU_BREAKPOINT);
+        this.site = 'Angular2 Material';
+        this.components = [];
+        this._subscription = null;
+        var query = media_1.Media.getQuery(DemosApp.SIDE_MENU_BREAKPOINT);
+        this._subscription = media.listen(query).onMatched.subscribe(function (mql) {
+            _this.fullPage = mql.matches;
+            _this.appRef.tick();
+        });
+        http.get('public/version.json')
+            .subscribe(function (res) {
+            _this.version = res.json().version;
+        });
+        this._components.getComponents()
+            .then(function (comps) {
+            _this.components = comps;
+        });
+    }
+    DemosApp.prototype.ngOnDestroy = function () {
+        this._subscription.unsubscribe();
+    };
+    DemosApp.prototype.showMenu = function (event) {
+        this._sidenav.show('menu');
+    };
+    DemosApp.prototype.navigate = function (to) {
+        this.router.navigate(to);
+    };
+    DemosApp.SIDE_MENU_BREAKPOINT = 'gt-md';
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Boolean)
+    ], DemosApp.prototype, "fullPage", void 0);
+    DemosApp = __decorate([
+        router_1.RouteConfig([
+            { path: '/', name: 'Index', component: index_1.IndexPage, useAsDefault: true },
+            { path: '/components/:id', name: 'Component', component: component_1.ComponentPage }
+        ]),
+        core_1.Component({
+            selector: 'demos-app',
+            host: {
+                '[class.push-menu]': 'fullPage'
+            }
+        }),
+        core_1.View({
+            templateUrl: 'examples/app.html',
+            directives: [all_1.MATERIAL_DIRECTIVES, router_1.ROUTER_DIRECTIVES, example_1.default, all_2.DEMO_DIRECTIVES]
+        }), 
+        __metadata('design:paramtypes', [http_1.Http, navigation_1.NavigationService, media_1.Media, router_1.Router, core_1.ApplicationRef, components_1.ComponentsService, sidenav_service_1.SidenavService])
+    ], DemosApp);
+    return DemosApp;
+})();
+exports.DemosApp = DemosApp;
+var appProviders = [
+    http_1.HTTP_PROVIDERS, router_1.ROUTER_PROVIDERS, all_1.MATERIAL_PROVIDERS,
+    components_1.ComponentsService, navigation_1.NavigationService, version_1.VersionService,
+    core_1.bind(router_1.LocationStrategy).toClass(router_1.HashLocationStrategy)
+];
+if (window.location.href.indexOf('github.com') !== -1) {
+    core_1.enableProdMode();
+}
+else {
+    appProviders.push(core_1.bind(view_listener_1.AppViewListener).toClass(common_dom_1.DebugElementViewListener));
+}
+browser_1.bootstrap(DemosApp, appProviders);
+//# sourceMappingURL=app.js.map
---
+++ v/0.2.1/ng2-material/components/dialog/dialog.ts
@@ -0,0 +1,132 @@
+import {
+  provide,
+  ComponentRef,
+  DynamicComponentLoader,
+  ElementRef,
+  Injectable,
+  ResolvedProvider,
+  Injector
+} from 'angular2/core';
+
+import {Promise} from 'angular2/src/facade/async';
+import {isPresent, Type} from 'angular2/src/facade/lang';
+import {MdDialogRef} from './dialog_ref';
+import {MdDialogConfig} from './dialog_config';
+import {MdDialogContainer} from './dialog_container';
+import {MdDialogBasic} from './dialog_basic';
+import {MdBackdrop} from "../backdrop/backdrop";
+import {DOM} from "angular2/src/platform/dom/dom_adapter";
+import {Renderer} from "angular2/core";
+import {Animate} from '../../core/util/animate';
+
+export * from './dialog_config';
+export * from './dialog_container';
+export * from './dialog_ref';
+export * from './dialog_basic';
+
+// TODO(jelbourn): body scrolling is disabled while dialog is open.
+// TODO(jelbourn): Don't manually construct and configure a DOM element. See #1402
+// TODO(jelbourn): Wrap focus from end of dialog back to the start. Blocked on #1251
+// TODO(jelbourn): Focus the dialog element when it is opened.
+// TODO(jelbourn): Pre-built `alert` and `confirm` dialogs.
+// TODO(jelbourn): Animate dialog out of / into opening element.
+
+/**
+ * Service for opening modal dialogs.
+ */
+@Injectable()
+export class MdDialog {
+  constructor(public componentLoader: DynamicComponentLoader,
+              public renderer: Renderer) {
+  }
+
+  private _defaultContainer = DOM.query('body');
+
+  /**
+   * Opens a modal dialog.
+   * @param type The component to open.
+   * @param elementRef The logical location into which the component will be opened.
+   * @param options
+   * @returns Promise for a reference to the dialog.
+   */
+  open(type: Type, elementRef: ElementRef, options: MdDialogConfig = null): Promise<MdDialogRef> {
+    var config = isPresent(options) ? options : new MdDialogConfig();
+
+
+    // Create the dialogRef here so that it can be injected into the content component.
+    var dialogRef = new MdDialogRef();
+
+    var bindings = Injector.resolve([provide(MdDialogRef, {useValue: dialogRef})]);
+
+    var backdropRefPromise = this._openBackdrop(elementRef, bindings, options);
+
+    // First, load the MdDialogContainer, into which the given component will be loaded.
+    return this.componentLoader.loadNextToLocation(MdDialogContainer, elementRef)
+      .then(containerRef => {
+        // TODO(tbosch): clean this up when we have custom renderers
+        // (https://github.com/angular/angular/issues/1807)
+        // TODO(jelbourn): Don't use direct DOM access. Need abstraction to create an element
+        // directly on the document body (also needed for web workers stuff).
+        // Create a DOM node to serve as a physical host element for the dialog.
+        var dialogElement = containerRef.location.nativeElement;
+
+        this.renderer.setElementClass(containerRef.location, 'md-dialog-absolute', !!config.container);
+
+        DOM.appendChild(config.container || this._defaultContainer, dialogElement);
+
+        if (isPresent(config.width)) {
+          this.renderer.setElementStyle(containerRef.location, 'width', config.width);
+        }
+        if (isPresent(config.height)) {
+          this.renderer.setElementStyle(containerRef.location, 'height', config.height);
+        }
+
+        dialogRef.containerRef = containerRef;
+
+        // Now load the given component into the MdDialogContainer.
+        return this.componentLoader.loadNextToLocation(type, containerRef.instance.contentRef, bindings)
+          .then((contentRef: ComponentRef) => {
+            Object.keys(config.context).forEach((key) => {
+              contentRef.instance[key] = config.context[key];
+            });
+
+            // Wrap both component refs for the container and the content so that we can return
+            // the `instance` of the content but the dispose method of the container back to the
+            // opener.
+            dialogRef.contentRef = contentRef;
+            containerRef.instance.dialogRef = dialogRef;
+
+            backdropRefPromise.then(backdropRef => {
+              dialogRef.backdropRef = backdropRef;
+              dialogRef.whenClosed.then((_) => {
+                backdropRef.dispose();
+              });
+            });
+
+            return Animate.enter(dialogElement, 'md-active').then(() => dialogRef);
+          });
+      });
+  }
+
+  /** Loads the dialog backdrop (transparent overlay over the rest of the page). */
+  _openBackdrop(elementRef: ElementRef, bindings: ResolvedProvider[], options: MdDialogConfig): Promise<ComponentRef> {
+    return this.componentLoader.loadNextToLocation(MdBackdrop, elementRef, bindings)
+      .then((componentRef) => {
+        let backdrop: MdBackdrop = componentRef.instance;
+        backdrop.clickClose = options.clickClose;
+        this.renderer.setElementClass(componentRef.location, 'md-backdrop', true);
+        this.renderer.setElementClass(componentRef.location, 'md-opaque', true);
+        this.renderer.setElementClass(componentRef.location, 'md-backdrop-absolute', !!options.container);
+        DOM.appendChild(options.container || this._defaultContainer, componentRef.location.nativeElement);
+        return backdrop.show().then(() => componentRef);
+      });
+  }
+
+  alert(message: string, okMessage: string): Promise<any> {
+    throw 'Not implemented';
+  }
+
+  confirm(message: string, okMessage: string, cancelMessage: string): Promise<any> {
+    throw 'Not implemented';
+  }
+}
