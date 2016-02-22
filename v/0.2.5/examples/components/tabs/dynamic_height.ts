6fcac4101db4053e446eaaadbe6162ca94f287f0
Justin DuJardin <justin@dujardinconsulting.com>
2016-02-21 16:53:44 -0800

    chore: v0.2.5

---
+++ v/0.2.5/examples/components/tabs/dynamic_height.ts
@@ -0,0 +1,12 @@
+import {View, Component, ViewEncapsulation} from 'angular2/core';
+import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
+
+@Component({selector: 'tabs-dynamic-height'})
+@View({
+  templateUrl: 'examples/components/tabs/dynamic_height.html',
+  styleUrls: ['examples/components/tabs/dynamic_height.css'],
+  directives: [MATERIAL_DIRECTIVES],
+  encapsulation: ViewEncapsulation.None
+})
+export default class TabsDynamicHeight {
+}
